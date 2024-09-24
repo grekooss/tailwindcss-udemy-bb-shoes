26e681894dd3af7babcc7bcac562669ec5367334
grekooss <grekoss@gmail.com>
2024-09-24 11:21:34 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/tailwindcss-udemy-bb-shoes/",
   server: {
     port: 3000,
   },
